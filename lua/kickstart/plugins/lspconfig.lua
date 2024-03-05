c31d1d97391f526fe511d01887727599f2ae3525
peder frang <peder.frang@gmail.com>
2024-03-06 00:21:20 +0100

    remove-clangd-format

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -132,10 +132,10 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {
-          cmd = { 'clangd', '--fallback-style={IndentWidth: 4}' },
-          -- filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
-        },
+        -- clangd = {
+        --   cmd = { 'clangd', '--fallback-style={IndentWidth: 4}' },
+        --   -- filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
+        -- },
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
