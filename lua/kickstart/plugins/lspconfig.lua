d424d66e99e5b120e5bbef80da4a90db6b329d5b
Peder Frang <peder.frang@gmail.com>
2024-04-06 22:42:45 +0200

    Sync clangd format

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -132,7 +132,11 @@ return {
             --  - settings (table): Override the default settings passed when initializing the server.
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
-                clangd = {},
+                clangd = { cmd = {
+                    'clangd',
+                    '--sync=1',
+                } },
+                -- clangd = {},
                 -- gopls = {},
                 pyright = {},
                 -- rust_analyzer = {},
